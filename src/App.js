9d7ee723fa112602656774634b04ad6017e1c16f
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-10-08 20:20:51 -0400

    added name

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 10151667 || Sakshat Garg || DevOps</h5>
+      <h5>Developed By: 10151667 || Sakshat Garg || DevOps || comp3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
